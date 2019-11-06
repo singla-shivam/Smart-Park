014bccda4e806e1d19eab5031d48a42c77524d53
nikeshthapa255 <nikeshthapa255@gmail.com>
2019-11-06 18:17:51 +0530

    now in typescript

---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>
+                Hello World
+            </div>
+          );
+    }
+}
+ 
+export default Main;
