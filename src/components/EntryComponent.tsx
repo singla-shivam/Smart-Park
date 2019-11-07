66a44b1ca7687a1e7a3a0d43b156aa5bf098d5f6
Shivam Singla <shivam.cse17@nituk.ac.in>
2019-11-07 10:34:56 +0530

    book

--- src/components/EntryComponent.tsx
+++ src/components/EntryComponent.tsx
@@ -71,12 +71,7 @@ class Entry extends React.Component<EntryProps, EntryState> {
         console.log(depDate, depDate - arrivalTimeStamp)
 
         const slotInfo: any = await this.props.firebase.getData(`info/slots`)
-
-        console.log(slotInfo)
-
-        const occupancy: number = (slotInfo[0].total - slotInfo[0].empty) / slotInfo[0].total
-
-        console.log(occupancy)
+        const emptiness: number = slotInfo[0].empty / slotInfo[0].total
         
 
         // TODO: calculate slot
@@ -84,7 +79,7 @@ class Entry extends React.Component<EntryProps, EntryState> {
         const slot = 'A01'
 
         const booking: BookingInterface = {
-            dynamicCharges: 1 - 2 * occupancy,
+            dynamicCharges: emptiness < 0.5 ? 1 - 2 * emptiness : 0,
             uid: vehicle[0].uid,
             vehNo: this.state.vehNo,
             slot: slot,
